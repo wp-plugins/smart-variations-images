5737dd18e87877108aeea001384bebb3bb0b88ad
drosendo <drosendo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-10 18:54:34 +0000

    Version 0.2 No longer use of Caption field, now it has is own field, added to the product images.
    git-svn-id: https://plugins.svn.wordpress.org/smart-variations-images/trunk@1196508 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: http://www.rosendo.pt/
 Tags: WooCommerce, images variations, gallery, woocommerce variations, woocommerce variations images, woocommerce images
 Requires at least: 3.0.1
 Tested up to: 4.2
-Stable tag: 0.1
+Stable tag: 0.2
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -21,7 +21,7 @@ This extension will allow the use of multiple images per variation, and simplifi
 Instead of upload one image per variation, upload all the variation images the product gallery and for each image CAPTION insert the slug of the variation.
 As quick and simple as that!
 
-Check out a demo at: http://www.rosendo.pt/produto/demo-smart-variations-images/
+Check out a demo at: http://www.rosendo.pt/en/product/demo-smart-variations-images/
 
 Take a look at the Screenshots section on how to work with it!
 
@@ -30,16 +30,23 @@ Take a look at the Screenshots section on how to work with it!
 
 1. Upload the entire `smart-variations-images` folder to the `/wp-content/plugins/` directory
 2. Activate the plugin through the 'Plugins' menu in WordPress
-3. Good luck with sales :)
+3. On your product assign the product attributes and save
+4. Go to Product Gallery and upload/choose your images
+5. Assing the slugs to be used for the variation images for each of the image and save.
+6. Good luck with sales :)
 
 == Screenshots ==
 
 1. Add images to your Product Gallery
-2. Choose the images to be used and add the "slug" of the variation to the "Caption" field.
+2. Choose the images to be used and select the "slug" of the variation in the "Variation Slug" field.
 3. Hides all other images that dont match the variation, and show only the default color, if no default is choosen, the gallery is hidden.
 4. On change the variation, images in the gallery also change to match the variation. The image in the gallery when click should show in the bigger image(above).
 
 == Changelog ==
+
+= 0.2 =
+No longer use of caption field for Variation, new field has been added to replace the caption.
+Javascript will search for new tag and loop the gallery.
 
 = 0.1 =
 Just released into the wild.
--- smart-variations-images.php
+++ smart-variations-images.php
@@ -5,7 +5,7 @@
   Plugin URI: http://www.rosendo.pt
   Description: This is a WooCommerce extension plugin, that allows the user to add any number of images to the product images gallery and be used as variable product variations images in a very simple and quick way, without having to insert images p/variation.
   Author: David Rosendo
-  Version: 0.1
+  Version: 0.2
   Author URI: http://www.rosendo.pt
  */
 
@@ -27,10 +27,85 @@ if (!class_exists('woo_svi')):
 
         function __construct() {
             add_action('wp_enqueue_scripts', array($this, 'woo_svi_scripts'), 100);
+            add_filter('attachment_fields_to_edit', array($this, 'woo_svi_field'), 10, 2);
+            add_filter('attachment_fields_to_save', array($this, 'woo_svi_field_save'), 10, 2);
+            add_filter('wp_get_attachment_image_attributes', array($this, 'add_woovsi_attribute'), 10, 2);
         }
 
         function woo_svi_scripts() {
             wp_enqueue_script('woo_svijs', plugin_dir_url(__FILE__) . 'assets/js/woo_svi.min.js', array('jquery'), null, true);
+        }
+
+        /**
+         * Add woovsi field to media uploader
+         *
+         * @param $form_fields array, fields to include in attachment form
+         * @param $post object, attachment record in database
+         * @return $form_fields, modified form fields
+         */
+        function woo_svi_field($form_fields, $post) {
+
+            $att = wc_get_attribute_taxonomy_names();
+            if (!empty($att)) {
+
+                $current = get_post_meta($post->ID, 'woosvi_slug', true);
+
+                $html = "<select name='attachments[{$post->ID}][woosvi-slug]' id='attachments[{$post->ID}][woosvi-slug]'>";
+
+                $variations = true;
+
+                foreach ($att as $value) {
+                    $html .="<option value='' " . selected($current, '', false) . ">none</option>";
+
+                    $fabric_values = get_the_terms($_POST['post_id'], $value);
+
+                    if (!empty($fabric_values)) {
+                        foreach ($fabric_values as $fabric_value) {
+                            $html .="<option value='" . $fabric_value->slug . "' " . selected($current, $fabric_value->slug, false) . ">" . $fabric_value->name . "</option>";
+                        }
+                    } else {
+                        $variations = false;
+                    }
+                }
+
+                $html .='</select>';
+
+                if ($variations) {
+                    $form_fields['woosvi-slug'] = array(
+                        'label' => 'Variation Slug',
+                        'input' => 'html',
+                        'html' => $html,
+                        'application' => 'image',
+                        'exclusions' => array('audio', 'video'),
+                        'helps' => 'Choose the variation Slug',
+                    );
+
+                    return $form_fields;
+                }
+            }
+        }
+
+        /**
+         * Save values of woovsi in media uploader
+         *
+         * @param $post array, the post data for database
+         * @param $attachment array, attachment fields from $_POST form
+         * @return $post array, modified post data
+         */
+        function woo_svi_field_save($post, $attachment) {
+            if (isset($attachment['woosvi-slug']))
+                update_post_meta($post['ID'], 'woosvi_slug', $attachment['woosvi-slug']);
+
+
+            return $post;
+        }
+
+        function add_woovsi_attribute($html, $post) {
+            $current = get_post_meta($post->ID, 'woosvi_slug', true);
+
+            $html['data-woovsi'] = $current;
+
+            return $html;
         }
 
     }
